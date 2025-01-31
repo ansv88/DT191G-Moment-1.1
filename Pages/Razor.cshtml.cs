fc912b146cdffe3434e460046afd9dc3963d23c5
Annelie Svensson <annelievs@hotmail.com>
2025-01-31 10:01:07 +0100

    Add project files.

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DT191G_Moment_1._1.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    //Visa knappen när användaren scrollat ner 100px
+    if (window.scrollY > 100) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
